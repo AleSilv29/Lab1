fd3e2358cc9d9e7e1db8e422d503f7e6895de8b6
Alexandra Silvasan <aleioana2002@gmail.com>
2022-10-11 19:34:41 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello you!");
+        Scanner scan = new Scanner(System.in);
+        //String s = scan.next();
+        int i;
+        float sum=0, ma, nr=0;
+        while(true)
+        {
+            i = scan.nextInt();
+            sum=sum+i;
+            nr++;
+            String Line = new String(scan.nextLine());
+            if(Line.length()==0) {
+                break;
+            }
+        }
+        ma=sum/nr;
+        System.out.println(ma);
     }
 }
